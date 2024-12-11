f95c850a5785905e816292145cece885d0b59410
richieieie <trungnguyen0803forwork@gmail.com>
2024-12-11 22:47:35 +0700

    Project Structure Set Up

---
+++ src/EShop.Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Marten;
+global using BuildingBlocks;
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
